9921d3351a7179a136d28af8ebc539843552d830
zhangyongwang <20225614@qq.com>
2019-10-28 15:11:03 +0800

    整理代码

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ function bind(asThis) {
     var args = slice.call(arguments, 1)
     var fn = this
     if (typeof fn !== 'function') {
-        // closest thing possible to the ECMAScript 5
-        // internal IsCallable function
         throw new Error('bind 必须用在函数身上');
     }
     return function () {
         var args2 = slice.call(arguments, 0)
         return fn.apply(asThis, args.concat(args2))
+    }
+}
+
+function _bind(asThis, ...args) {
+    // this 就是函数
+    const fn = this
+    return function (...args2) {
+        return fn.call(asThis, ...args, ...args2)
     }
 }
 
--- test/index.js
+++ test/index.js
@@ -1,23 +1,40 @@
 const bind = require("../src/index")
-Function.prototype.bind2 = bind
-console.assert(Function.prototype.bind2 !== undefined)
 
-const fn1 = function () {
-    return this
+function test1(message) {
+    Function.prototype.bind2 = bind
+    console.assert(Function.prototype.bind2 !== undefined)
+    console.log(message)
 }
-const newFn1 = fn1.bind2({ name: 'frank' })
-console.assert(newFn1().name === 'frank')
 
-
-const fn2 = function (p1, p2) {
-    return [this, p1, p2]
+function test2(message) {
+    Function.prototype.bind2 = bind
+    const fn1 = function () { return this }
+    const newFn1 = fn1.bind2({ name: 'frank' })
+    console.assert(newFn1().name === 'frank')
+    console.log(message)
 }
-const newFn2 = fn2.bind2({ name: 'frank' }, 123, 456)
-console.assert(newFn2()[0].name === 'frank', "this")
-console.assert(newFn2()[1] === 123, "p1")
-console.assert(newFn2()[2] === 456, "p2")
 
-const anotherFn2 = fn2.bind2({ name: 'frank' }, 444)
-console.assert(anotherFn2(245)[0].name === 'frank',"this")
-console.assert(anotherFn2(245)[1] === 444,"p1")
-console.assert(anotherFn2(245)[2] === 245,"p22")
+function test3(message) {
+    Function.prototype.bind2 = bind
+    const fn2 = function (p1, p2) { return [this, p1, p2] }
+    const newFn2 = fn2.bind2({ name: 'frank' }, 123, 456)
+    console.assert(newFn2()[0].name === 'frank', "this")
+    console.assert(newFn2()[1] === 123, "p1")
+    console.assert(newFn2()[2] === 456, "p2")
+    console.log(message)
+}
+
+function test4(message) {
+    Function.prototype.bind2 = bind
+    const fn2 = function (p1, p2) { return [this, p1, p2] }
+    const anotherFn2 = fn2.bind2({ name: 'frank' }, 444)
+    console.assert(anotherFn2(245)[0].name === 'frank', "this")
+    console.assert(anotherFn2(245)[1] === 444, "p1")
+    console.assert(anotherFn2(245)[2] === 245, "p22")
+    console.log(message)
+}
+
+test1("fn.bind 能用")
+test2("this 绑定能用")
+test3("this,p1,p2 绑定成功")
+test4("this,p1绑定成功，后传p2调用成功")
