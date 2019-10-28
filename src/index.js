3096b4e88d79908a16ea507dd2664b3125356f54
zhangyongwang <20225614@qq.com>
2019-10-28 14:23:37 +0800

    用旧的语法写一遍

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-function bind(asThis, ...args) {
+var slice = Array.prototype.slice;
+function bind(asThis) {
     // this 就是函数
-    const fn = this
-    return function (...args2) {
-        return fn.call(asThis, ...args, ...args2)
+    var args = slice.call(arguments, 1)
+    var fn = this
+    if (typeof fn !== 'function') {
+        // closest thing possible to the ECMAScript 5
+        // internal IsCallable function
+        throw new Error('bind 必须用在函数身上');
+    }
+    return function () {
+        var args2 = slice.call(arguments, 0)
+        return fn.apply(asThis, args.concat(args2))
     }
 }
 
